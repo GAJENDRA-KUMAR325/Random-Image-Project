73c7dde3d22dac18a497ac47794cfddc7089e59d
GAJENDRA-KUMAR325 <124331510+GAJENDRA-KUMAR325@users.noreply.github.com>
2023-06-28 19:28:14 +0530

    Add files via upload

---
+++ Random Photo/index.js
@@ -0,0 +1,12 @@
+const imageContainerEl = document.querySelector(".image-container");
+const btnEl = document.querySelector(".btn");
+btnEl.addEventListener("click",()=>{
+    addNewImages();
+})
+function addNewImages(){
+    for(let i=0;i<30;i++){
+        const newImgEl = document.createElement("img");
+    newImgEl.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`
+    imageContainerEl.appendChild(newImgEl);
+    }
+}
